d8d00a02a4a1e109f65e7c1d6a13c3d9fb80d291
Dinolavr <Dinolavr@users.noreply.github.com>
2019-12-15 22:55:48 +0500

    Pod

---
+++ DotaStat/UIView+Constraints.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+Constraints.swift
+//  DotaStat
+//
+//  Created by MacBook Pro on 11.12.2019.
+//  Copyright © 2019 MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func pinEdgesToSuperView() {
+        guard let superView = superview else { return }
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leftAnchor.constraint(equalTo: superView.leftAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+        rightAnchor.constraint(equalTo: superView.rightAnchor).isActive = true
+    }
+}
